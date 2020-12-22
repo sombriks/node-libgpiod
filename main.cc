84739dcc4465adff781435f1ad6155e657b660f4
Leonardo Silveira <sombriks@gmail.com>
2020-12-22 20:47:36 -0300

    get parameters on c++ side, thanks Nan!

--- main.cc
+++ main.cc
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <gpiod.h>
 
-#include <nan.h> 
+#include <nan.h>
 
-NAN_METHOD(listChips) {
-  struct gpiod_chip *chip = NULL;
-
-  chip = gpiod_chip_open("/dev/gpiochip0");
-
-  if(chip) {
-    struct gpiod_line *line = gpiod_chip_get_line(chip, 1);
-
-    int v = gpiod_line_get_value(line);
-
-    printf("hello, line 1 is %d\n",v);
-  } else {
-    printf("unable to open chip!\n");
-  }
+NAN_METHOD(version)
+{
+  info.GetReturnValue()
+      .Set(Nan::New<v8::String>(gpiod_version_string())
+               .ToLocalChecked());
 }
 
-NAN_MODULE_INIT(InitAll) {
-  Nan::Set(target, Nan::New("listChips").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(listChips)).ToLocalChecked());
+NAN_METHOD(getLine)
+{
+  Nan::Utf8String device(info[0]);
+  unsigned int offset = Nan::To<unsigned int>(info[1]).FromJust();
+  bool active_low = Nan::To<bool>(info[2]).FromJust();
+  char *consumer = NULL;
+
+  int value = gpiod_ctxless_get_value(*device, offset, active_low, consumer);
+
+  info.GetReturnValue().Set(Nan::New<v8::Integer>(value));
+
+}
+
+NAN_MODULE_INIT(InitAll)
+{
+  Nan::Set(target, Nan::New("version").ToLocalChecked(),
+           Nan::GetFunction(Nan::New<v8::FunctionTemplate>(version)).ToLocalChecked());
+  Nan::Set(target, Nan::New("getLine").ToLocalChecked(),
+           Nan::GetFunction(Nan::New<v8::FunctionTemplate>(getLine)).ToLocalChecked());
 }
 
 NODE_MODULE(NativeExtension, InitAll)
--- test/index.js
+++ test/index.js
@@ -1,11 +1,23 @@
+const gpiod = require("../");
 
-const gpiod = require("../")
+describe("libgpiod gindings", function () {
+  it("should get libgpiod version", function (done) {
+    console.log(gpiod.version());
+    done();
+  });
 
+  it("should get line value by chip number", function (done) {
+    console.log(gpiod.getLine(0, 0));
+    done();
+  });
 
-describe('libgpiod gindings', function() {
+  it("should get line value by chip name", function (done) {
+    console.log(gpiod.getLine("gpiochip0", 0));
+    done();
+  });
 
-  it("should list chips", function(done){
-    gpiod.listChips()
-    done()
-  })
-})
+  it("should get line value by chip path", function (done) {
+    console.log(gpiod.getLine("/dev/gpiochip0", 0));
+    done();
+  });
+});
