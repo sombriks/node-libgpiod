50750ceb5ad50bbeeb8d60ed4736884bd7bfae22
Leonardo Silveira <sombriks@gmail.com>
2020-12-22 20:49:34 -0300

    improve test case abit more

--- test/index.js
+++ test/index.js
@@ -11,13 +11,18 @@ describe("libgpiod gindings", function () {
     done();
   });
 
-  it("should get line value by chip name", function (done) {
+  it("should get line 1 value by chip name", function (done) {
     console.log(gpiod.getLine("gpiochip0", 0));
     done();
   });
 
-  it("should get line value by chip path", function (done) {
+  it("should get line 1 value by chip path", function (done) {
     console.log(gpiod.getLine("/dev/gpiochip0", 0));
+    done();
+  });
+
+  it("should NOT get line 1 value due wrong chip name", function (done) {
+    console.log(gpiod.getLine("/dev/gpiochipZero", 0));
     done();
   });
 });
