8ca1a6ae8221408a6842fa5946607eec94ad506b
Leonardo Silveira <sombriks@gmail.com>
2020-12-29 20:04:36 -0300

    still working on test case

--- test/00-misc.js
+++ test/00-misc.js
@@ -1,5 +1,7 @@
 const gpiod = require("..");
 
+const assert = require("assert");
+
 describe("libgpiod miscellaneous bindings", function () {
   it("should get libgpiod version", function (done) {
     console.log(gpiod.version());
@@ -22,15 +24,16 @@ describe("libgpiod miscellaneous bindings", function () {
   });
 
   it("should NOT get line instant value due wrong chip name", function (done) {
-    console.log(gpiod.getInstantLineValue("/dev/gpiochipZero", 17));
+    const ret = gpiod.getInstantLineValue("/dev/gpiochipZero", 17);
+    assert(ret == -1, "chip should not exists");
     done();
   });
 
-  it("should set line instant value to zero and back to 1", function (done) {
-    gpiod.setInstantLineValue("/dev/gpiochip0", 17, 0);
+  it("should blink line instant value", function (done) {
+    gpiod.setInstantLineValue("/dev/gpiochip0", 17, 1);
     console.log(gpiod.getInstantLineValue(0, 17));
     setTimeout(() => {
-      gpiod.setInstantLineValue("/dev/gpiochip0", 17, 1);
+      gpiod.setInstantLineValue("/dev/gpiochip0", 17, 0);
       console.log(gpiod.getInstantLineValue(0, 17));
       done();
     }, 500);
