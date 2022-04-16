d7c43da865edb737789d2b29eb0cddbee1594b82
Christoph Engelbert (noctarius) <me@noctarius.com>
2022-04-16 13:00:45 +0200

    added check to prevent trying to load the libgpio native module on anything other than linux

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// prevent module being loaded on anything other than Linux
+const os = require('os');
+if (os.type() !== 'Linux') {
+    throw new Error('libgpiod is only available on Linux!');
+}
+
 // entry point
 const libgpiod = require('bindings')('node-libgpiod');
 
